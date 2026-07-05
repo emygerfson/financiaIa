f0231d5a2541f6d33c6332cc5ed15b7836d6a5dd
emygerfson <emy.silva1404@gmail.com>
2026-07-04 21:21:09 -0300

    Termino dos botão e aguardando a interce

---
+++ financeiro/src/components/shared/Input.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import type { InputHTMLAttributes } from 'react';
+
+import { Divider } from './Divider';
+
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    prefix?: string;
+    suffix?: string;
+}
+export function Input({prefix, suffix, ...rest}: InputProps){
+    return (
+        <div className='bg-input flex items-center rounded-2xl p-4 shadow[4px_4px_18px_0px_rgba(0,0,0,0.2)]'>
+            {prefix &&(
+                <>
+                    <span className='text-muted-foreground txt-sm font-medium'>
+                        {prefix}
+                        
+                    </span>
+                    <Divider orientation="vertical"/>
+                </>
+            )}
+            <input 
+                className='text-foreground placeholder:text-muted-foreground w-full bg-transparent outline-none' 
+                autoFocus
+                {...rest}
+            />
+            {suffix &&(
+                <>
+                <Divider orientation="vertical"/>
+                <span className='text-muted-foreground txt-sm font-medium'>
+                    {suffix}
+
+                </span>
+                </>
+            )}
+             
+
+
+            
+
+        </div>
+    )
+}
--- financeiro/src/features/Simulation/Form.tsx
+++ financeiro/src/features/Simulation/Form.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { StepProgress } from "../../features/Simulation/Progress";
+import { FormStep } from "./FormStep";
+import { PiggyBank } from "lucide-react";
 
 export function SimulationForm() {
     return(
         <>
             <StepProgress currentStep={1} totalSteps={6} />
+            <FormStep
+                 icon={PiggyBank} 
+                 title="Renda Mensal Bruta" 
+                 question="Quanto e depositado na sua conta mensalmente(some todas as fontes)?"
+                 inputProps={{
+                    type: "text",
+                    placeholder: "ex: 5.000,00",
+                    prefix: "R$",
+                 }}
+            />
         </>
     )
 }
